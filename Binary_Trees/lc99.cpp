e5f87c7e40795a4913d044539ad96fe55f71c5a7
Vishal <vishal.a.060904@gmail.com>
2025-06-25 12:26:14 +0000

    Optimal lc99

--- Binary_Trees/lc99.cpp
+++ Binary_Trees/lc99.cpp
@@ -14,33 +14,6 @@ struct TreeNode{
     }
 };
 
-class BSTIterator {
-private:
-    stack<TreeNode*> st;
-    void pushleft(TreeNode* root, stack<TreeNode*>& st){
-        if (root == nullptr) return;
-        while (root != nullptr){
-            st.push(root);
-            root = root->left;
-        }
-    }
-public:
-    BSTIterator(TreeNode* root) {
-        pushleft(root, st);
-    }
-    
-    TreeNode* next() {
-        TreeNode* temp = st.top();
-        st.pop();
-        if (temp->right != nullptr) pushleft(temp->right, st);
-        return temp;
-    }
-    
-    bool hasNext() {
-        return (!st.empty());
-    }
-};
-
 // Brute Force
 // Get the inorder traversal
 // Sort it
@@ -70,3 +43,40 @@ void recoverTree(TreeNode* root) {
 
 // TC - O(N) + O(NlogN) + O(N)
 // SC - O(N) + O(N) + O(N)
+
+
+//Optimal
+// During inorder Traversal, keep a hold of the prev element as well
+// Core idea - Inorder should be sorted, so iterate through it tho find where it is not sorted, ans
+// just swap the values.
+
+TreeNode* startNode = nullptr;
+TreeNode* middleNode = nullptr;
+TreeNode* endNode = nullptr;
+
+void getSwapNode(TreeNode* root, TreeNode* &prev){
+    if (root == nullptr) return;
+    getSwapNode(root->left, prev);
+    if (prev != nullptr){
+        if (root->val < prev->val){
+            if (startNode == nullptr && middleNode == nullptr){
+                startNode = prev;
+                middleNode = root;
+            }
+            else endNode = root;
+        }
+    }
+    prev = root;
+    getSwapNode(root->right,prev);
+}
+
+void recoverTree(TreeNode* root) {
+    TreeNode* prev = nullptr;
+    startNode = middleNode = endNode = nullptr;
+    getSwapNode(root, prev);
+    if (endNode != nullptr) swap(startNode->val, endNode->val);
+    else swap(startNode->val, middleNode->val);
+}
+
+//TC - O(N)
+//SC - O(N) recursive stack space
